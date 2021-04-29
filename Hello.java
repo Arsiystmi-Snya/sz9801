2a66d8f4edc5f3b1b49b6c1146fe790c9c7c5de2
Arsiystmi-Snya <heartbaner516@gmail.com>
2021-04-29 17:23:21 +0800

    1 冲突提交

--- Hello.java
+++ Hello.java
@@ -2,8 +2,8 @@ public class Hello{
 	
 	public static void main(String[] args){
 		System.out.println("111111");
-		System.out.println("222222");
-		System.out.println("333333");
-		System.out.println("444444");
+		System.out.println("woshi");
+		System.out.println("第一");
+		System.out.println("第二");
 	}
 }
